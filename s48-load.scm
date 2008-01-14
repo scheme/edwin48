ab7461b7b273118797034fdefcb2971d19f4277d
Duncan Mak <duncan@a-chinaman.com>
2008-01-14 16:11:35 -0500

    Add scheme48 load file

---
+++ s48-load.scm
@@ -0,0 +1,24 @@
+;;; -*- Mode: Scheme; scheme48-package: (exec) -*-
+;;;
+;;; Copyright © 2007 Duncan Mak <duncan@ccs.neu.edu>
+;;;
+;;; This code is placed in the Public Domain.  All warranties are
+;;; disclaimed.
+;;;
+;;; load.scm - Script for loading Edgar
+;;;
+
+(user)
+
+(config '(load "s48-let-opt.scm"
+               "s48-packages.scm"
+               "interfaces.scm"
+               "packages.scm"))
+
+(open 'literals)
+;; (open 'edwin-groups)
+;; (open 'edwin-marks)
+;; (open 'edwin-regions)
+;; (open 'edwin-motion)
+
+
