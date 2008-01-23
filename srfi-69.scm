6a71dbcfdb540fb483908a7e25f48ad704cb1c44
Duncan Mak <duncan@a-chinaman.com>
2008-01-23 14:22:54 -0500

    Implement a subset of SRFI-69 hash tables using the tables module.
    The following procedures were not implemented:
    
    hash-table-equivalence-function
    hash-table-hash-function
    hash-table-update!
    hash-table-update!/default
    hash-table-fold
    hash-table-copy
    hash-table-merge!
    string-ci-hash
    hash-by-identity

---
+++ srfi-69.scm
@@ -0,0 +1,64 @@
+;;; -*- Mode: Scheme; scheme48-package: srfi-69 -*-
+;;;
+;;; SRFI 69: Basic hash tables
+;;;
+
+(define* (make-hash-table (equal? equal?) (hash hash))
+  ((make-table-maker equal? hash)))
+
+(define (hash-table? t) (table? t))
+
+(define* (alist->hash-table alist (equal? equal?) (hash hash))
+  (let ((table (make-hash-table equal? hash)))
+    (for-each (lambda (x) (hash-table-set! table (car x) (cdr x)))
+              alist)
+    table))
+
+(define (hash-table-ref table key . thunk)
+  (let ((value (table-ref table key)))
+    (if (eq? value #f)
+        (if (null? thunk) (error "key not found") (thunk))
+        value)))
+
+(define (hash-table-ref/default table key default)
+  (hash-table-ref table key (lambda () default)))
+
+(define (hash-table-set! table key value)
+  (table-set! table key value))
+
+(define (hash-table-delete! table key)
+  (table-set! table key #f))
+
+(define (hash-table-exists? table key)
+  (hash-table-ref table key (lambda () #f)))
+
+(define (hash-table-size table)
+  (let ((size 0))
+    (table-walk
+     (lambda (k v) (set! size (+ size 1)))
+     table)
+    size))
+
+(define (hash-table-keys table)
+  (let ((keys '()))
+    (table-walk
+     (lambda (k v) (set! keys (cons k keys)))
+     table)
+    keys))
+
+(define (hash-table-values table)
+  (let ((values '()))
+    (table-walk
+     (lambda (k v) (set! values (cons v values)))
+     table)
+    values))
+
+(define (hash-table-walk table proc)
+  (table-walk proc table))
+
+(define (hash-table->alist table)
+  (let ((alist '()))
+    (table-walk
+     (lambda (k v) (set! alist (cons (list k v) alist)))
+     table)
+    alist))
--- srfi-packages.scm
+++ srfi-packages.scm
@@ -42,3 +42,27 @@
         byte-vectors
         (subset primitives (copy-bytes!)))
   (files srfi-66))
+
+
+;;; SRFI 69: Basic hash tables
+;;;
+;;; Implement this using the tables module
+
+(define-interface srfi-69-interface
+  (export make-hash-table hash-table? alist->hash-table
+          ;; hash-table-equivalence-function, hash-table-hash-function
+          hash-table-ref hash-table-ref/default
+          hash-table-set! hash-table-delete! hash-table-exists?
+          ;; hash-table-update! hash-table-update!/default
+          hash-table-size hash-table-keys hash-table-values
+          hash-table-walk ;; hash-table-fold
+          hash-table->alist ;; hash-table-copy hash-table-merge!
+          hash string-hash ;; string-ci-hash hash-by-identity
+          ))
+
+(define-structure srfi-69 srfi-69-interface
+  (open scheme
+        (subset signals (error))
+        define-opt
+        (modify tables (rename (default-hash-function hash))))
+  (files srfi-69))
