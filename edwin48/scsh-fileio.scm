a6a579787e8a6238f501d930ba795834fc53eb7d
Duncan Mak <duncan@a-chinaman.com>
2008-06-06 05:15:47 -0400

    Remove Scheme48 implementation of FILE-EXISTS? in the Scsh implementation.

--- edwin48/scsh-fileio.scm
+++ edwin48/scsh-fileio.scm
@@ -1,11 +1,7 @@
 ;;; -*- mode: scheme; scheme48-package: mit-fileio -*-
 
-(define (file-exists? filename)
-  (accessible? filename (access-mode exists)))
-
 (define (file-eq? filename1 filename2)
   (let ((info1 (file-info filename1))
         (info2 (file-info filename2)))
     (= (file-info:inode info1)
        (file-info:inode info2))))
-,
