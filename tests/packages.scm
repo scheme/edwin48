15d39362c3714f9c9edbab0e678502667e5704fd
Duncan Mak <duncan@a-chinaman.com>
2008-10-09 17:05:53 -0400

    Use the actual implementation of GET-CONSOLE-INPUT-OPERATIONS instead of writing my own input loop.
    Oddly enough, key sequences are still not recognized.

--- tests/packages.scm
+++ tests/packages.scm
@@ -13,7 +13,7 @@
 
 (define-structure keystroke-printer
     (export start-printer)
-  (open scheme
-        aliases fixnum keystroke terminal-mode terminfo
-        srfi-13)
+  (open scheme scsh-subset
+        aliases ascii fixnum formats io-support keystroke keystroke-modifiers terminal-mode terminfo
+        srfi-8 srfi-13 srfi-23)
   (files keystroke-printer))
