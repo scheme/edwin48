17c02dcf9bdc172bad37fb73871afbc31e837a3f
Crewbie <crewbie@bigtv.ccs.neu.edu>
2010-07-20 15:25:56 -0400

    Cleaned scratch-interfaces. Added groups, marks, and regions tests.

---
+++ scratch/scratch-load.scm
@@ -0,0 +1,34 @@
+;;; -*- Mode: Scheme; scheme48-package: (exec) -*-
+;;;
+;;; Copyright © 2007 Duncan Mak <duncan@ccs.neu.edu>
+;;;
+;;; This code is placed in the Public Domain.  All warranties are
+;;; disclaimed.
+;;;
+;;; load.scm - Script for loading Edgar
+;;;
+
+(user)
+
+(translate "=base" "..")
+
+(config '(load "=base/config-macros.scm"))
+
+(config '(load "=base/terminfo/interfaces.scm"
+               "=base/terminfo/scsh-packages.scm"))
+
+(config '(load "=base/soosy/interfaces.scm"
+               "=base/soosy/packages.scm"))
+
+(config '(load "=base/edwin48/srfi-packages.scm"
+               "=base/edwin48/scsh/packages.scm"))
+
+(config '(load "=base/edwin48/scratch-interfaces.scm"
+	       "=base/edwin48/scratch-packages.scm"))
+
+(config '(load "test-groups.scm"))
+
+
+
+
+
---
+++ scratch/test-groups.scm
@@ -0,0 +1,75 @@
+(define-structure test-groups
+  (export run-test)
+  (open scheme
+	formats
+	srfi-13
+	edwin:group
+	edwin:region
+	edwin:mark
+	edwin:buffer
+	edwin:motion)
+  (begin
+
+    (define (print . stuff)
+      (for-each
+       (lambda (s) (display s) (newline))
+       stuff))
+
+    (define (print-group group)
+      (print
+       (group-extract-string group
+			     (group-start-index group)
+			     (group-end-index group))))
+
+    (define (run-test)
+      (newline)
+      (display "Should print \"Hello\": ")
+      (test-group-insert-char)
+      (newline)
+
+      (display "Should print \"Hello World.\": ")
+      (test-group-region-transform)
+      (newline)
+
+      (display "Should print \"Hello!(x3)\":  ")
+      (test-region-insert)
+      (newline))
+      
+      
+
+    (define (test-group-insert-char)
+      (let* ((buffer (make-buffer))
+	     (group  (make-group buffer)))
+	(set-buffer-group! buffer group)
+	(group-insert-char! (buffer-group buffer) 0 #\H)
+	(group-insert-char! (buffer-group buffer) 1 #\e)
+	(group-insert-char! (buffer-group buffer) 2 #\l)
+	(group-insert-char! (buffer-group buffer) 3 #\l)
+	(group-insert-char! (buffer-group buffer) 4 #\o)
+	(print-group group)))
+
+    (define (test-group-region-transform)
+      (let* ((buffer (make-buffer))
+	     (group (make-group buffer))
+	     (_ (set-buffer-group! buffer group))
+	     (_ (group-insert-string! group 0 "Hello dlroW."))
+	     (left-mark (make-mark group 6))
+	     (right-mark (make-mark group 11))
+	     (region (make-region left-mark right-mark))
+	     (_ (region-transform! region string-reverse)))
+	(print-group group)))
+
+    (define (test-region-insert)
+      (let* ((buffer (make-buffer))
+	     (group (make-group buffer))
+	     (_ (set-buffer-group! buffer group))
+	     (_ (group-insert-string! group 0 "Hello!"))
+	     (left-mark (make-mark group 0))
+	     (right-mark (make-mark group 6))
+	     (region (make-region left-mark right-mark)))
+	(region-insert! right-mark region)
+	(region-insert! right-mark region)
+	(region-insert! right-mark region)
+	(print-group group)))
+    )
+  )
