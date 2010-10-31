f3a894452ceecc028985c4e0d5f2799866846669
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-10-31 18:45:35 -0400

    Start bringing the tests up to date. Still a WIP.

--- tests/load.scm
+++ tests/load.scm
@@ -2,8 +2,10 @@
 
 (translate "=base" "..")
 
-;;; EDWIN-EXPORT
 (config '(load "=base/config-macros.scm"))
+
+(config '(load "=base/edwin48/srfi-packages.scm"
+               "=base/edwin48/scsh/packages.scm"))
 
 (config '(load "=base/terminfo/interfaces.scm"
                "=base/terminfo/scsh-packages.scm"))
@@ -11,8 +13,10 @@
 (config '(load "=base/soosy/interfaces.scm"
                "=base/soosy/packages.scm"))
 
-(config '(load "=base/edwin48/srfi-packages.scm"
-               "=base/edwin48/scsh/packages.scm"
-               "=base/edwin48/interfaces.scm"))
+(config '(load "=base/pantene/edwin-interfaces.scm"
+               "=base/pantene/edwin-packages.scm"))
+
+(config '(load "=base/edwin48/scratch-interfaces.scm"
+               "=base/edwin48/scratch-packages.scm"))
 
 (config '(load "packages.scm"))
